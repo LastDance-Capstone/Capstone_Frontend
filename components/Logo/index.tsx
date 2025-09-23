d1e21d6031f4e5fce31688531f3942955ae56904
김도훈 <dohun0310@naver.com>
2025-09-23 17:07:06 +0900

    [Design/#1] Refactor: `Logo` 컴포넌트의 모바일 크기 조정

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -8,8 +8,8 @@ export default function Logo({
 }: LogoProps) {
   return type === "icon" ? (
     <svg
-      width={size * 0.96969696969}
-      height={size}
+      width={size * 1.03125}
+      height={size * 0.96969696969}
       viewBox="0 0 64 64"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
